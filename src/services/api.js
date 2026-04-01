91083de27df1d8b781b872d9f448be15f1ee73fb
Bind Pratap Singh <158027240+bindpratapsingh@users.noreply.github.com>
2026-04-01 07:10:47 +0530

    Switch API baseURL to Render URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
     // Temporarily switch back to your local Java server!
-    baseURL: 'http://localhost:8080/api', 
+    //baseURL: 'http://localhost:8080/api', 
     
     // (Keep your Render URL commented out below so you don't lose it)
-    // baseURL: 'https://vrsms-backend.onrender.com/api', 
+     baseURL: 'https://vrsms-backend.onrender.com/api', 
 });
 
 export default api;
